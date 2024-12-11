e93de6830e2ff22c77491ac3ade3177680bbeaa6
Leoooii <lioneh39@gmail.com>
2024-12-11 10:00:48 +0200

    10.12 laptop - AUTH si logici 5. inca nu s-a spart. the cope is real

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,13 +1,17 @@
 "use client";
 
 import React, { Suspense } from "react";
-import Auth from "@/components/Sections/Auth";
+import dynamic from "next/dynamic";
 import { Spinner } from "@nextui-org/spinner";
+
+const Auth = dynamic(() => import("@/components/Sections/Auth"), {
+  ssr: false,
+});
 
 const Page = () => {
   return (
-    <div className={" w-full  p-5 flex flex-col flex-1 gap-5"}>
-      <div className="border-white b-2 rounded-md bg-white  p-5">
+    <div className="w-full p-5 flex flex-col flex-1 gap-5">
+      <div className="border-white b-2 rounded-md bg-white p-5">
         <Suspense fallback={<Spinner color="warning" label="Loading..." />}>
           <Auth />
         </Suspense>
@@ -19,4 +23,5 @@ const Page = () => {
     </div>
   );
 };
+
 export default Page;
